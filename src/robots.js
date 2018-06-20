5e2b5a96760e553c1f43081701dd9d4fe6f8a669
Your Name <acidduckling@gmail.com>
2018-06-20 22:30:41 +1000

    Create initial card

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Erwin Howell',
+    username: 'Antonette',
+    email: 'shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Clementine Buch',
+    username: 'Samantha',
+    email: 'nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.Oconner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'sincere@april.biz'
+  },
+  {
+    id: 6,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'sincere@april.biz'
+  },
+  {
+    id: 7,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'sincere@april.biz'
+  },
+  {
+    id: 8,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'sincere@april.biz'
+  },
+  {
+    id: 9,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'sincere@april.biz'
+  },
+  {
+    id: 10,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'sincere@april.biz'
+  },
+]
