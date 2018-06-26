657abe0e0b6108db35cde5386764713703409803
Your Name <acidduckling@gmail.com>
2018-06-26 12:50:16 +1000

    Implemented ErrorBoundary

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,15 +2,13 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  
   return (
     <div>
-      {
-        robots.map((robot, i) => {
-          return <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email} />;
-        })
-      }
-    </div>);
+      {robots.map((robot, i) => {
+        return <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email} />;
+      })}
+    </div>
+  );
 };
 
 export default CardList;
