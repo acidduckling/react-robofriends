aea44c2f18e8f116fa56aab723fc1dc172ac1a35
Your Name <acidduckling@gmail.com>
2018-06-26 12:10:46 +1000

    Tidy up folders

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import 'tachyons';
-import App from './App';
+import App from './containers/App';
 
 React.createElement();
 ReactDOM.render(<App />, document.getElementById('root'));
